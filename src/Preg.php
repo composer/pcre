01a712383af57c47b058a355be7de799ff0e3431
Jordi Boggiano <j.boggiano@seld.be>
2021-12-02 21:19:14 +0100

    Reorder slightly

--- src/Preg.php
+++ src/Preg.php
@@ -16,47 +16,6 @@ class Preg
     const ARRAY_MSG = '$subject as an array is not supported. You can use \'foreach\' instead.';
 
     /**
-     * @param string|string[] $pattern
-     * @param string|string[] $replacement
-     * @param string          $subject
-     * @param int             $limit
-     * @param int             $count Set by method
-     * @return string
-     */
-    public static function filter($pattern, $replacement, $subject, $limit, &$count)
-    {
-        if (is_array($subject)) { // @phpstan-ignore-line
-            throw new \InvalidArgumentException(static::ARRAY_MSG);
-        }
-
-        $result = preg_filter($pattern, $replacement, $subject, $limit, $count);
-
-        // Keep phpstan happy. See: https://github.com/phpstan/phpstan-src/pull/826
-        // Can be replaced with `$result === null` when this has been fixed
-        if (!is_string($result)) {
-            throw PcreException::fromFunction('preg_filter', $pattern);
-        }
-
-        return $result;
-    }
-
-    /**
-     * @param string   $pattern
-     * @param string[] $array
-     * @param int      $flags
-     * @return string[]
-     */
-    public static function grep($pattern, array $array, $flags = 0)
-    {
-        $result = preg_grep($pattern, $array, $flags);
-        if ($result === false) {
-            throw PcreException::fromFunction('preg_grep', $pattern);
-        }
-
-        return $result;
-    }
-
-    /**
      * @param string   $pattern
      * @param string   $subject
      * @param array<string|null> $matches Set by method
@@ -174,8 +133,8 @@ class Preg
     /**
      * @param string $pattern
      * @param string $subject
-     * @param int $limit
-     * @param int $flags
+     * @param int    $limit
+     * @param int    $flags
      * @return string[]
      */
     public static function split($pattern, $subject, $limit = -1, $flags = 0)
@@ -183,6 +142,47 @@ class Preg
         $result = preg_split($pattern, $subject, $limit, $flags);
         if ($result === false) {
             throw PcreException::fromFunction('preg_split', $pattern);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string|string[] $pattern
+     * @param string|string[] $replacement
+     * @param string          $subject
+     * @param int             $limit
+     * @param int             $count Set by method
+     * @return string
+     */
+    public static function filter($pattern, $replacement, $subject, $limit, &$count)
+    {
+        if (is_array($subject)) { // @phpstan-ignore-line
+            throw new \InvalidArgumentException(static::ARRAY_MSG);
+        }
+
+        $result = preg_filter($pattern, $replacement, $subject, $limit, $count);
+
+        // Keep phpstan happy. See: https://github.com/phpstan/phpstan-src/pull/826
+        // Can be replaced with `$result === null` when this has been fixed
+        if (!is_string($result)) {
+            throw PcreException::fromFunction('preg_filter', $pattern);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string   $pattern
+     * @param string[] $array
+     * @param int      $flags
+     * @return string[]
+     */
+    public static function grep($pattern, array $array, $flags = 0)
+    {
+        $result = preg_grep($pattern, $array, $flags);
+        if ($result === false) {
+            throw PcreException::fromFunction('preg_grep', $pattern);
         }
 
         return $result;
