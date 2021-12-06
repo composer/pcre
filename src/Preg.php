fcf270ae715c734d2f2409fe5bd690ec31b7fa3c
Jordi Boggiano <j.boggiano@seld.be>
2021-12-06 14:15:11 +0100

    Add splitWithOffset for PREG_SPLIT_OFFSET_CAPTURE

--- src/Preg.php
+++ src/Preg.php
@@ -189,12 +189,34 @@ class Preg
      * @param string $pattern
      * @param string $subject
      * @param int    $limit
-     * @param int    $flags
+     * @param int    $flags PREG_SPLIT_NO_EMPTY or PREG_SPLIT_DELIM_CAPTURE
      * @return list<string>
      */
     public static function split($pattern, $subject, $limit = -1, $flags = 0)
     {
+        if (($flags & PREG_SPLIT_OFFSET_CAPTURE) !== 0) {
+            throw new \InvalidArgumentException('PREG_SPLIT_OFFSET_CAPTURE is not supported as it changes the type of $matches, use splitWithOffset() instead');
+        }
+
         $result = preg_split($pattern, $subject, $limit, $flags);
+        if ($result === false) {
+            throw PcreException::fromFunction('preg_split', $pattern);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string $pattern
+     * @param string $subject
+     * @param int    $limit
+     * @param int    $flags PREG_SPLIT_NO_EMPTY or PREG_SPLIT_DELIM_CAPTURE
+     * @return list<array{string, int}>
+     * @phpstan-return list<array{string, int<0, max>}>
+     */
+    public static function splitWithOffset($pattern, $subject, $limit = -1, $flags = 0)
+    {
+        $result = preg_split($pattern, $subject, $limit, $flags | PREG_SPLIT_OFFSET_CAPTURE);
         if ($result === false) {
             throw PcreException::fromFunction('preg_split', $pattern);
         }
