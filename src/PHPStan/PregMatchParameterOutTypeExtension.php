a83519c92b20f4f670229ad838c99311449c6e33
Markus Staab <maggus.staab@googlemail.com>
2024-06-22 16:39:47 +0200

    reflect flags behaviour

--- src/PHPStan/PregMatchParameterOutTypeExtension.php
+++ src/PHPStan/PregMatchParameterOutTypeExtension.php
@@ -1,63 +1,57 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
 namespace Composer\Pcre\PHPStan;
 
 use Composer\Pcre\Preg;
-use PhpParser\Node\Expr\FuncCall;
-use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\StaticCall;
 use PHPStan\Analyser\Scope;
-use PHPStan\Reflection\FunctionReflection;
 use PHPStan\Reflection\MethodReflection;
 use PHPStan\Reflection\ParameterReflection;
 use PHPStan\TrinaryLogic;
-use PHPStan\Type\MethodParameterOutTypeExtension;
 use PHPStan\Type\Php\RegexArrayShapeMatcher;
 use PHPStan\Type\StaticMethodParameterOutTypeExtension;
 use PHPStan\Type\Type;
-use function in_array;
-use function strtolower;
 
 final class PregMatchParameterOutTypeExtension implements StaticMethodParameterOutTypeExtension
 {
 
     private RegexArrayShapeMatcher $regexShapeMatcher;
-	public function __construct(
-		RegexArrayShapeMatcher $regexShapeMatcher
-	)
-	{
+
+    public function __construct(
+        RegexArrayShapeMatcher $regexShapeMatcher
+    )
+    {
         $this->regexShapeMatcher = $regexShapeMatcher;
-	}
+    }
 
     public function isStaticMethodSupported(MethodReflection $methodReflection, ParameterReflection $parameter): bool
     {
         return
             $methodReflection->getDeclaringClass()->getName() === Preg::class
             && $methodReflection->getName() === 'match'
-            && $parameter->getName() === 'matches'
-        ;
+            && $parameter->getName() === 'matches';
     }
 
     public function getParameterOutTypeFromStaticMethodCall(MethodReflection $methodReflection, StaticCall $methodCall, ParameterReflection $parameter, Scope $scope): ?Type
     {
-		$args = $methodCall->getArgs();
-		$patternArg = $args[0] ?? null;
-		$matchesArg = $args[2] ?? null;
-		$flagsArg = $args[3] ?? null;
+        $args = $methodCall->getArgs();
+        $patternArg = $args[0] ?? null;
+        $matchesArg = $args[2] ?? null;
+        $flagsArg = $args[3] ?? null;
 
-		if (
-			$patternArg === null || $matchesArg === null
-		) {
-			return null;
-		}
+        if (
+            $patternArg === null || $matchesArg === null
+        ) {
+            return null;
+        }
 
-		$patternType = $scope->getType($patternArg->value);
-		$flagsType = null;
-		if ($flagsArg !== null) {
-			$flagsType = $scope->getType($flagsArg->value);
-		}
+        $flagsType = PregMatchFlags::getType($flagsArg, $scope);
+        if ($flagsType === null) {
+            return null;
+        }
+        $patternType = $scope->getType($patternArg->value);
 
-		return $this->regexShapeMatcher->matchType($patternType, $flagsType, TrinaryLogic::createMaybe());
-	}
+        return $this->regexShapeMatcher->matchType($patternType, $flagsType, TrinaryLogic::createMaybe());
+    }
 
 }
--- src/PHPStan/PregMatchTypeSpecifyingExtension.php
+++ src/PHPStan/PregMatchTypeSpecifyingExtension.php
@@ -1,30 +1,23 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
 namespace Composer\Pcre\PHPStan;
 
 use Composer\Pcre\Preg;
-use PhpParser\Node\Expr\FuncCall;
-use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\StaticCall;
 use PHPStan\Analyser\Scope;
 use PHPStan\Analyser\SpecifiedTypes;
 use PHPStan\Analyser\TypeSpecifier;
 use PHPStan\Analyser\TypeSpecifierAwareExtension;
 use PHPStan\Analyser\TypeSpecifierContext;
-use PHPStan\Reflection\FunctionReflection;
 use PHPStan\Reflection\MethodReflection;
 use PHPStan\TrinaryLogic;
-use PHPStan\Type\FunctionTypeSpecifyingExtension;
-use PHPStan\Type\MethodTypeSpecifyingExtension;
 use PHPStan\Type\Php\RegexArrayShapeMatcher;
 use PHPStan\Type\StaticMethodTypeSpecifyingExtension;
-use function in_array;
-use function strtolower;
 
 final class PregMatchTypeSpecifyingExtension implements StaticMethodTypeSpecifyingExtension, TypeSpecifierAwareExtension
 {
 
-	private TypeSpecifier $typeSpecifier;
+    private TypeSpecifier $typeSpecifier;
 
     private RegexArrayShapeMatcher $regexShapeMatcher;
 
@@ -36,58 +29,59 @@ final class PregMatchTypeSpecifyingExtension implements StaticMethodTypeSpecifyi
     }
 
 
-	public function setTypeSpecifier(TypeSpecifier $typeSpecifier): void
-	{
-		$this->typeSpecifier = $typeSpecifier;
-	}
+    public function setTypeSpecifier(TypeSpecifier $typeSpecifier): void
+    {
+        $this->typeSpecifier = $typeSpecifier;
+    }
 
-    public function getClass(): string {
+    public function getClass(): string
+    {
         return Preg::class;
     }
 
-    public function isStaticMethodSupported(MethodReflection $methodReflection, StaticCall $node, TypeSpecifierContext $context) : bool
+    public function isStaticMethodSupported(MethodReflection $methodReflection, StaticCall $node, TypeSpecifierContext $context): bool
     {
         return $methodReflection->getName() === 'match' && !$context->null();
     }
 
-    public function specifyTypes(MethodReflection $methodReflection, StaticCall $node, Scope $scope, TypeSpecifierContext $context) : SpecifiedTypes
+    public function specifyTypes(MethodReflection $methodReflection, StaticCall $node, Scope $scope, TypeSpecifierContext $context): SpecifiedTypes
     {
-		$args = $node->getArgs();
-		$patternArg = $args[0] ?? null;
-		$matchesArg = $args[2] ?? null;
-		$flagsArg = $args[3] ?? null;
+        $args = $node->getArgs();
+        $patternArg = $args[0] ?? null;
+        $matchesArg = $args[2] ?? null;
+        $flagsArg = $args[3] ?? null;
 
-		if (
-			$patternArg === null || $matchesArg === null
-		) {
-			return new SpecifiedTypes();
-		}
+        if (
+            $patternArg === null || $matchesArg === null
+        ) {
+            return new SpecifiedTypes();
+        }
 
-		$patternType = $scope->getType($patternArg->value);
-		$flagsType = null;
-		if ($flagsArg !== null) {
-			$flagsType = $scope->getType($flagsArg->value);
-		}
+        $flagsType = PregMatchFlags::getType($flagsArg, $scope);
+        if ($flagsType === null) {
+            return new SpecifiedTypes();
+        }
+        $patternType = $scope->getType($patternArg->value);
 
-		$matchedType = $this->regexShapeMatcher->matchType($patternType, $flagsType, TrinaryLogic::createFromBoolean($context->true()));
-		if ($matchedType === null) {
-			return new SpecifiedTypes();
-		}
+        $matchedType = $this->regexShapeMatcher->matchType($patternType, $flagsType, TrinaryLogic::createFromBoolean($context->true()));
+        if ($matchedType === null) {
+            return new SpecifiedTypes();
+        }
 
-		$overwrite = false;
-		if ($context->false()) {
-			$overwrite = true;
-			$context = $context->negate();
-		}
+        $overwrite = false;
+        if ($context->false()) {
+            $overwrite = true;
+            $context = $context->negate();
+        }
 
-		return $this->typeSpecifier->create(
-			$matchesArg->value,
-			$matchedType,
-			$context,
-			$overwrite,
-			$scope,
-			$node,
-		);
-	}
+        return $this->typeSpecifier->create(
+            $matchesArg->value,
+            $matchedType,
+            $context,
+            $overwrite,
+            $scope,
+            $node,
+        );
+    }
 
 }
