92f975f539a7a9736f51f2f22b11aa140b7ea3cb
Jordi Boggiano <j.boggiano@seld.be>
2024-07-11 15:26:26 +0200

    Fix example to work around phpstan bug for now

--- tests/PHPStanTests/nsrt/preg-match.php
+++ tests/PHPStanTests/nsrt/preg-match.php
@@ -28,12 +28,12 @@ function doMatch(string $s): void
     }
     assertType('array{}|array{0: string, 1?: string|null}', $matches);
 
-    if (Preg::isMatch('/Price: (?P<currency>£|€)\d+/', $s, $matches)) {
-        assertType('array{0: string, 1: string|null, currency: string|null}', $matches);
+    if (Preg::isMatch('/Price: (?<currency>£|€)\d+/', $s, $matches)) {
+        assertType('array{0: string, currency: string|null, 1: string|null}', $matches);
     } else {
         assertType('array{}', $matches);
     }
-    assertType('array{}|array{0: string, 1?: string|null}', $matches);
+    assertType('array{}|array{0: string, currency: string|null, 1: string|null}', $matches);
 }
 
 // disabled until https://github.com/phpstan/phpstan-src/pull/3185 can be resolved
